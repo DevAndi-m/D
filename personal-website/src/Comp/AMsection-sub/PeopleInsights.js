e92e08a477d2ef688d9b6ac8335d12791b995a2b
DevAndi-m <andimorina093@gmail.com>
2024-08-03 02:15:55 +0200

    #

--- personal-website/src/Comp/AMsection-sub/PeopleInsights.js
+++ personal-website/src/Comp/AMsection-sub/PeopleInsights.js
@@ -4,6 +4,9 @@ import smiler from '../images/smiling-man_1098-15443.jpg';
 import { StarIcon } from './AboutIcons';
 
 function PeopleInsights() {
+
+  const starSize = 30
+
   return (
     <div className='cardMain'>
       <div className='rpd'>
@@ -17,11 +20,11 @@ function PeopleInsights() {
         If a ParallaxLayer with the sticky property passed is wrapped in a div or fragment, the element disappears for the time it should be "sticking" on screen. Removing any enclosing div/fragments returns to correct behavior. This is a problem on a larger site I'm developing where I would like to split some of the layers up into subcomponents, which necessitates being able to include extra divs/fragments. To Reproduce Put a fragment around a ParallaxLayer w sticky property set. It can enclose more than just that one ParallaxLayer but the problem shows up w just the one enclosed. The sticky element is visible before and after its designated sticky phase, but disappears during.
       </p>
       <div className='starHolder'>
-        <p><StarIcon size={30} /></p>
-        <p><StarIcon size={30} /></p>
-        <p><StarIcon size={30} /></p>
-        <p><StarIcon size={30} /></p>
-        <p><StarIcon size={30} /></p>
+        <p><StarIcon size={starSize} /></p>
+        <p><StarIcon size={starSize} /></p>
+        <p><StarIcon size={starSize} /></p>
+        <p><StarIcon size={starSize} /></p>
+        <p><StarIcon size={starSize} /></p>
       </div>
     </div>
   )
